d20532e769083e3536bb15cc1ab02579f239540c
Aleksandr Panchul <apanchul@hotmail.com>
2018-08-25 22:20:40 -0400

    quick build-in type demo

---
+++ limits/integers.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "biggest integer on a 16-bit machine is \"" << 0xffff << "\"\n"; 
+	cout << "                 (which is, if signed, \"" << (short int)0xffff << "\")\n"; 
+	cout << "                   so for signed it is \"" << (short int)0x7fff << "\"\n"; 
+	cout << "                     1 + that would be \"" << (short int)(0x7fff + 1) << "\"\n"; 
+
+	return 0;
+}
