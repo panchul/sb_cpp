e54efe4f017375c6670ff3e96db6020a4a4dddd2
Alex Panchul <alek8106@gmail.com>
2020-07-18 04:56:08 -0400

    minor tweaks

--- cuda/add_plain.cpp
+++ cuda/add_plain.cpp
@@ -1,40 +1,38 @@
-// from here: https://devblogs.nvidia.com/even-easier-introduction-cuda/
-// pre-req: get the toolkit
 //
-// $ clang++ add_plain.cpp -o add_plain
+// ??? $ clang++ add_plain.cpp -o add_plain
+// $ g++ add_plain.cpp -o add_plain
 // $ ./add_plain
 // Max error: 0.000000
 //
 #include <iostream>
 #include <math.h>
 
-// function to add the elements of two arrays
-void add(int n, float *x, float *y) {
-  for (int i = 0; i < n; i++)
-      y[i] = x[i] + y[i];
+void add(int n, float *x, float *y, float *z) {
+  for (int i = 0; i < n; i++) {
+      z[i] = x[i] + y[i];
+  }
 }
+
 int main(void) {
-  int N = 1<<20; // 1M elements
+  int N = 1 << 30;
   float *x = new float[N];
   float *y = new float[N];
+  float *z = new float[N];
 
-  // initialize x and y arrays on the host
   for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
+    x[i] = i;
+    y[i] = -i;
   }
 
-  // Run kernel on 1M elements on the CPU
-  add(N, x, y);
+  add(N, x, y, z);
 
-  // Check for errors (all values should be 3.0f)
   float maxError = 0.0f;
   for (int i = 0; i < N; i++)
-    maxError = fmax(maxError, fabs(y[i]-3.0f));
+    maxError = fmax(maxError, fabs(z[i]-0.0f));
   std::cout << "Max error: " << maxError << std::endl;
 
-  // Free memory
   delete [] x;
   delete [] y;
+  delete [] z;
   return 0;
 }
